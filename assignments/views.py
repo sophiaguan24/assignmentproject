87acc0bbfe11af435099533fe89747db2c55eaef
Sophia Guan <sophia.guan.24@bishops.com>
2022-05-16 10:17:20 -0700

    URLS added and Login Page View

--- assignments/views.py
+++ assignments/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import View
+from django.utils import timezone
+
 from django.shortcuts import render
+class IndexView(View):
+    def get(self,request):
+        return render(request, 'assignments/login.html')
 
 # Create your views here.
