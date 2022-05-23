3761b1a37946cf788ff941bdfcaee5eb0768e234
Sophia Guan <sophia.guan.24@bishops.com>
2022-05-22 18:49:53 -0700

    login page

--- assignments/urls.py
+++ assignments/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.IndexView, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('assignments/<username>',views.AssignmentView.as_view(),name='assignments'),
     path('assignments/<username>/make',views.MakeView.as_view(),name='make')
 ]
--- assignments/views.py
+++ assignments/views.py
@@ -4,20 +4,39 @@ from django.urls import reverse
 from django.views import View
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from assignments.models import Assignment
-# def IndexView(request):
-#     if request.POST:
-#         if 'inputUsername' in request.POST.keys():
-#             user = authenticate(username=request.POST['inputUsername'],
-#                 password=request.POST['inputPassword'])
-#                 if user is not None:
-#                     login(request,user)
-#             else:
-#                 pass
-#         elif 'logout' in request.POST.keys():
-#             logout(request)
+
+class IndexView(View):
+    def get(self, request):
+        # We'll use the built-in authenticaion form
+        form = AuthenticationForm()
+        # Send the template the form and the posts.
+        context = {
+            'form': form,
+            'user': request.user,
+        }
+        return render(request, 'assignments/login.html', context)
+    def post(self,request):
+        if 'logout' in request.POST.keys():
+            logout(request)
+            form = AuthenticationForm()
+        else:
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(username = username, password = password)
+                if user is not None:
+                    login(request, user = user)
+        context = {
+            'form': form
+        }
+        return render(request, 'assignments/login.html', context)
+
 class AssignmentView(View):
     # @login_required
     def get(self, request, username):
